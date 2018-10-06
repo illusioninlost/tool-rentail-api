b142d107c465e803c16f1cd9b61564d353b69c42
illusioninlost <rchang9999@yahoo.com>
2018-10-05 20:39:50 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Tool.create(name:"hammer",description:"solid 20' hammer", price:"4.00", url:"https://i.imgur.com/Bq7dbSe.jpg")
+Tool.create(name:"miscellaneous",description:"miscellaneous tools such as hammer-like ax", price:"10.00", url:"https://i.imgur.com/u71CbDg.jpg")
+Tool.create(name:"angle saw",description:"14' AEG Abrasive Circular Saw, banana shaped 14' Rigid pipe wrench for scale.", price:"20.00", url:"https://i.imgur.com/PVlmSUo.jpg")
+Tool.create(name:"knife",description:"15' curved knife", price:"2.75", url:"https://i.imgur.com/zOJOsPJ.jpg")
+Tool.create(name:"knife",description:"butterfly knife", price:"1.00", url:"https://i.imgur.com/NbFG5Fx.jpg")
+Tool.create(name:"drill",description:"black&decker power drill with a short cord", price:"25.00", url:"https://i.imgur.com/5r1am9i.png")
+Tool.create(name:"handsaw",description:"30' wooden-handled handsaw", price:"12.00", url:"https://i.imgur.com/UQd5dW6.jpg")
+Tool.create(name:"rackets set",description:"40' tall set with ratchets", price:"20.00", url:"https://i.imgur.com/N9PJdtS.jpg")
+Tool.create(name:"ladder",description:"wooden ladder", price:"9.99", url:"https://i.imgur.com/rCRvjl4.png")
